8255c4c85004da59b7d21946baf44af6b5f03714
Chubacca <ruven@orderaheadapp.com>
2014-05-04 13:36:00 -0700

    index only relevant ones

--- doc_search/search_indexes.py
+++ doc_search/search_indexes.py
@@ -10,7 +10,7 @@ class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
         return Document
 
     def index_queryset(self, using=None):
-        return self.get_model().objects.all()
+        return self.get_model().objects.filter(indexed=True)
 
     def prepare_tags(self, obj):
         return [tag.text for tag in obj.tags.all()]
