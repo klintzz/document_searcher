9ee29c8b6fee40a9ae70d12cf7ec65616c43dc4a
Chubacca <ruven@orderaheadapp.com>
2014-05-04 12:36:42 -0700

    new search backend

--- document_searcher/settings.py
+++ document_searcher/settings.py
@@ -72,8 +72,11 @@ DATABASES = {
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        # 'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
 
