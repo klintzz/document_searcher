c8177f281c0c5a99318bc2e845610f6c282c7e9f
Chubacca <ruven@orderaheadapp.com>
2014-05-10 11:44:33 -0700

    auth stuff

--- doc_search/views.py
+++ doc_search/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from haystack.views import SearchView
+from django.shortcuts import redirect
+
+class ProtectedSearchView(SearchView):
+
+    def __call__(self, request):
+        if not request.user.is_authenticated():
+            return redirect('/login/?next=%s' % request.path)
+        return super(ProtectedSearchView, self).__call__(request)
--- document_searcher/urls.py
+++ document_searcher/urls.py
@@ -6,15 +6,15 @@ admin.autodiscover()
 from haystack.forms import SearchForm
 from haystack.views import SearchView
 
+from doc_search.views import ProtectedSearchView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'document_searcher.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^search/', include('haystack.urls')),
-    url(r'^$', SearchView(
-        template='search/search.html',
-        form_class=SearchForm
-    ), name='haystack_search'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    # url(r'^search/', include('haystack.urls')),
+    url(r'^$', ProtectedSearchView(template='search/search.html', form_class=SearchForm), name='haystack_search'),
 )
