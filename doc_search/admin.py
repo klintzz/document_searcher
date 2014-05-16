d388c462889a5e0839a4c15b9fcd0a559c2f7e09
Chubacca <ruven@orderaheadapp.com>
2014-05-16 11:43:39 -0700

    indexing changes

--- doc_search/admin.py
+++ doc_search/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from doc_search.models import Document, Tag
+from doc_search.models import Document
 
 admin.site.register(Document)
-admin.site.register(Tag)
+# admin.site.register(Tag)
 # Register your models here.
--- doc_search/search_indexes.py
+++ doc_search/search_indexes.py
@@ -3,8 +3,8 @@ from doc_search.models import Document
 
 class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    tags = indexes.CharField(model_attr='tags', null=True)
-    #document_text = indexes.CharField(model_attr='document_text')
+    # tags = indexes.CharField(model_attr='tags', null=True)
+    # document_text = indexes.CharField(model_attr='document_text')
 
     def get_model(self):
         return Document
@@ -12,5 +12,5 @@ class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(indexed=True)
 
-    def prepare_tags(self, obj):
-        return [tag.text for tag in obj.tags.all()]
+    # def prepare_tags(self, obj):
+    #     return [tag.text for tag in obj.tags.all()]
