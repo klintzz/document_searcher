c717a75146554e1d53387ee6a64cec7029583611
root <root@ip-172-31-6-101.us-west-2.compute.internal>
2014-05-16 01:11:25 +0000

    new models

--- doc_search/models.py
+++ doc_search/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
-class Tag(models.Model):
-    def __unicode__(self):
-        return self.text
-
-    text = models.CharField(max_length=200, unique=True, db_index=True)
+#class Tag(models.Model):
+#    def __unicode__(self):
+#        return self.text
+#
+#    text = models.CharField(max_length=200, unique=True, db_index=True)
 
 class Document(models.Model):
     def __unicode__(self):
         return self.file_name
 
     file_name = models.CharField(max_length=32, unique=True, db_index=True)
-    # document_text = models.TextField()
-    tags = models.ManyToManyField(Tag)
+    document_text = models.TextField()
+    # tags = models.ManyToManyField(Tag)
     done = models.BooleanField(default=False)
     indexed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=true)
+    search_indexed = models.DateTimeField(null=True)
