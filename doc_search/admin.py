2112c8246541add894f2ff59450be323ef07b8d9
Chubacca <ruven@orderaheadapp.com>
2014-05-27 13:56:52 -0700

    more admin stuff

--- doc_search/admin.py
+++ doc_search/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from doc_search.models import Document
 
-admin.site.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'done', 'indexed', 'created', 'search_indexed')
+
+
+admin.site.register(Document, DocumentAdmin)
 # admin.site.register(Tag)
 # Register your models here.
